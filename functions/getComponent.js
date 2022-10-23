af0a3bf9ab8a7c27a153c507ff6582def20cca4c
Henrique <henriqueferreira.fox@gmail.com>
2022-10-23 20:49:39 -0300

    fix redirects

--- functions/getComponent.js
+++ functions/getComponent.js
@@ -1,7 +1,7 @@
 export default async function getComponent(componentName) {
   try {
     const query = await fetch(
-      `/components/${componentName}/${componentName}.html`
+      `./components/${componentName}/${componentName}.html`
     );
     return query.text();
   } catch (e) {
